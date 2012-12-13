7bb7ab292b4b74060c2bf678986ec4a22de2c06e
Enno Runne <enno@runne.net>
2012-12-13 19:31:01 +0100

    reader with main method

--- server/src/main/java/dataserver/CassandraRead.java
+++ server/src/main/java/dataserver/CassandraRead.java
@@ -11,17 +11,27 @@ import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
 import com.netflix.astyanax.model.Column;
 import com.netflix.astyanax.model.ColumnFamily;
 import com.netflix.astyanax.model.ColumnList;
-import com.netflix.astyanax.serializers.DoubleSerializer;
 import com.netflix.astyanax.serializers.LongSerializer;
 import com.netflix.astyanax.serializers.StringSerializer;
 import com.netflix.astyanax.thrift.ThriftFamilyFactory;
 
 public class CassandraRead {
+	
+	public static void main(String[] args) throws ConnectionException {
+		CassandraRead me = new CassandraRead();
+		Keyspace ks = me.init();
+		ColumnFamily<String, Long> cf = me.getCf();
+		ColumnList<Long> columns = me.readRow(ks, cf, "hj");
+		for (Column<Long> c : columns) {
+			System.out.println(c.getName());
+		}
 
+	}
+	
 	public Keyspace init() {
 		AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder()
-				.forCluster("ClusterName")
-				.forKeyspace("KeyspaceName")
+				.forCluster("JavaJava")
+				.forKeyspace("JavaKeyspaceName")
 				.withAstyanaxConfiguration(
 						new AstyanaxConfigurationImpl()
 								.setDiscoveryType(NodeDiscoveryType.NONE))
@@ -45,19 +55,12 @@ public class CassandraRead {
 		return CF_USER_INFO;
 	}
 
-	public void readRow(Keyspace ks, ColumnFamily<String, Long> cf, String rowKey) throws ConnectionException {
-		OperationResult<ColumnList<Long>> result = ks
-				.prepareQuery(cf).getKey(rowKey).execute();
+	public ColumnList<Long> readRow(Keyspace ks, ColumnFamily<String, Long> cf,
+			String rowKey) throws ConnectionException {
+		OperationResult<ColumnList<Long>> result = ks.prepareQuery(cf)
+				.getKey(rowKey).execute();
 		ColumnList<Long> columns = result.getResult();
-
-		// Lookup columns in response by name
-//		int age = columns.getColumnByName("age").getIntegerValue();
-//		long counter = columns.getColumnByName("loginCount").getLongValue();
-//		String address = columns.getColumnByName("address").getStringValue();
-
-		// Or, iterate through the columns
-		for (Column<Long> c : columns) {
-			System.out.println(c.getName());
-		}
+		return columns;
 	}
+	
 }
